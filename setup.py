64af9ace6c7725701bfc588ebb2358abcc04da94
Alecktos <persson33@gmail.com>
2015-10-05 22:11:22 +0200

    making it a package, adding info to setup

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ __author__ = 'alexander.persson'
 from setuptools import setup
 
 setup(name='filemover',
-      version='1.0',
+      version='0.1dev',
+      packages=['Directory-Tree-File-Mover',],
       author='Alexander Persson',
       url='https://github.com/Alecktos/Directory-Tree-File-Mover',
       license='MIT'
+
 )
