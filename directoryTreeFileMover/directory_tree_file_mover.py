cdaa526bd7fefae608c51aceb70d4af6114a5083
Alecktos <vitt17!peal>
2015-10-11 15:53:49 +0200

    Running script with arguments instead

--- directoryTreeFileMover/directory_tree_file_mover.py
+++ directoryTreeFileMover/directory_tree_file_mover.py
@@ -9,27 +9,36 @@ __author__ = 'alexander.persson'
 
 def main():
     show_name = sys.argv[1]
+
+    if 2 in sys.argv:
+        if sys.argv[2] == '-f':
+            force_create_folders = True
+        else:
+            force_create_folders = False
+
     destination_root = 'destination'
     file_matcher = FileMatcher()
     file_paths = file_matcher.search_files(show_name, 'sourcefolder')
+    show_destination = destination_root + '/' + show_name
 
     try:
         file_mover = FileMover()
-        show_destination = destination_root + '/' + show_name
         file_mover.move_files(file_paths, destination_root, show_name) #TODO: borde vara move_files
 
     except CouldNotFindShowFolderException:
-        question = 'Show folder does not exist. Create ' + show_destination + '? (yes/no)'
-        user_input = raw_input(question)
-        if user_input.lower().strip() is 'yes':  # TODO: refactored into module
+        if force_create_folders:
+            print 'Show folder does not exist. Creating ' + show_destination
             FileHandler.create_dir(show_destination)
-            logger.log('Show folder created ' + show_destination)
+            logger.log(show_destination + ' created')
+        else:
+            print 'Show folder does not exist. Runt with -f to create it'
 
     except CouldNotFindSeasonFolderException, error:
         season_path = show_destination + '/Season ' + error.season_number
 
-        question = 'Season folder does not exist. Create ' + show_destination + '? (yes/no)'
-        user_input = raw_input(question)
-        if user_input.lower().strip() is 'yes':  # TODO: refactored into module
+        if force_create_folders:
+            print 'Season folder does not exist. Creating ' + season_path
             FileHandler.create_dir(season_path)
-            logger.log('Season folder created ' + season_path)
+            logger.log(season_path + ' created')
+        else:
+            print season_path + ' does not exist. Run with -f to create it'
--- tests/test_file_matcher.py
+++ tests/test_file_matcher.py
@@ -6,16 +6,16 @@ from directoryTreeFileMover.file_handler import FileHandler
 class FileMatcherTest(unittest.TestCase):
 
     TEST_SEARCH_IN_DIRECTORY_PATH = 'testfolder'
-    TEST_FILE_PATH = TEST_SEARCH_IN_DIRECTORY_PATH + 'The.Big.Bang.Theory.S09E01.HDTV.x264-LOL[rarbg].mp4'
+    TEST_FILE_PATH = TEST_SEARCH_IN_DIRECTORY_PATH + '/The.Big.Bang.Theory.S09E01.HDTV.x264-LOL[rarbg].mp4'
 
     def setUp(self):
         self.__file_mather = FileMatcher()
         self.__file_handler = FileHandler()
         self.__file_handler.create_dir(self.TEST_SEARCH_IN_DIRECTORY_PATH)
+        self.__file_handler.create_file(self.TEST_FILE_PATH)
 
     def tearDown(self):
         self.__file_handler.delete_directory(self.TEST_SEARCH_IN_DIRECTORY_PATH)
-        self.__file_handler.create_file(self.TEST_FILE_PATH)
 
     def runTest(self):
         search_for = 'The Big Bang Theory'
