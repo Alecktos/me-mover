9e4d064ff0712760d1edba762dec3fa9024aa646
Alecktos <persson33@gmail.com>
2015-10-21 20:08:06 +0200

    fixed tests and documentations

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ file names needs to be specified with S01E05 to be moved correctly. The files wi
 ## 2. Move episodes with episode-mover
 ### Example:
     episode-mover -show-name "Halt And Catch Fire" -source "media/inbox-tv-shows" -destination "media/sorted-tv-shows" -force
-If there are for example a file named Halt.and.Catch.Fire.S02E08.mp4 in media/inbox-tv-shows it will be moved to media/sorted-tv-shows/Halt And Catch Fire/Season 2/Halt.and.Catch.Fire.S02E08.mp4. If that directory do not exist et will be created.
+If there for example are a file named Halt.and.Catch.Fire.S02E08.mp4 in media/inbox-tv-shows it will be moved to media/sorted-tv-shows/Halt And Catch Fire/Season 2/Halt.and.Catch.Fire.S02E08.mp4. If that directory does not exist it will be created.
 
 ### Commands:
-    -force              when running script with force command show name directory and season directory will be created if they do not exist.
-    -show-name          Name fo the show to search for.
-    -source             Directory in witch to look for episode files to be moved.
-    -destination        The root directory that find episodes should be moved into.
+    -force              Show name directory and season directory will be created if they do not exist.
+    -show-name          Name of the show to search for.
+    -source             Directory in witch to look for episode-files to move.
+    -destination        Root directory that found episodes should be moved into.
--- tests/test_episode_mover.py
+++ tests/test_episode_mover.py
@@ -10,13 +10,13 @@ class FileMatcherTest(unittest.TestCase):
 
     TEST_SEARCH_IN_DIRECTORY_PATH = 'sourcefolder'
     TEST_DESTINATION_ROOT_FOLDER = 'destination'
-
-    TEST_FILE_PATH = TEST_SEARCH_IN_DIRECTORY_PATH + '/Halt.and.Catch.Fire.S02E10.720p.HDTV.x264-KILLERS.mkv'
+    TEST_FILE_NAME = '/Halt.and.Catch.Fire.S02E10.720p.HDTV.x264-KILLERS.mkv'
+    TEST_FILE_SOURCE_PATH = TEST_SEARCH_IN_DIRECTORY_PATH + TEST_FILE_NAME
 
     def setUp(self):
         file_handler.create_dir(self.TEST_SEARCH_IN_DIRECTORY_PATH)
         file_handler.create_dir(self.TEST_DESTINATION_ROOT_FOLDER)
-        file_handler.create_file(self.TEST_FILE_PATH)
+        file_handler.create_file(self.TEST_FILE_SOURCE_PATH)
 
     def tearDown(self):
         file_handler.delete_directory(self.TEST_SEARCH_IN_DIRECTORY_PATH)
@@ -24,8 +24,19 @@ class FileMatcherTest(unittest.TestCase):
 
     def runTest(self):
         self.__run_app('-show-name "halt and catch fire" -force -source sourcefolder -destination destination')
+        self.__assert_file_in_new_path()
+        self.__assert_file_not_in_old_path()
 
     def __run_app(self, args):
         p = subprocess.Popen('python -m episodeMover ' + args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         for line in p.stdout.readlines():
             print line,
+
+    def __assert_file_in_new_path(self):
+        destination_path = self.TEST_DESTINATION_ROOT_FOLDER + '/halt and catch fire/Season 2' + self.TEST_FILE_NAME
+        file_is_in_new_path = file_handler.check_file_existance(destination_path)
+        self.assertTrue(file_is_in_new_path)
+
+    def __assert_file_not_in_old_path(self):
+        file_is_in_old_path = file_handler.check_file_existance(self.TEST_FILE_SOURCE_PATH)
+        self.assertFalse(file_is_in_old_path)
