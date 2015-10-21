a097bfb89e168c91c46afa659b114aa8adbd40b6
Alecktos <persson33@gmail.com>
2015-10-21 20:48:43 +0200

    application name changed

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ __author__ = 'alexander.persson'
 
 from setuptools import setup
 
-setup(name='episodeMover',
+setup(name='episode-mover',
       version='0.1dev',
       packages=['episodeMover'],
       author='Alexander Persson',
       url='https://github.com/Alecktos/Directory-Tree-File-Mover',
       license='MIT',
       entry_points={
-            'console_scripts': ['episodeMover = episodeMover.directory_tree_file_mover:main']
+            'console_scripts': ['episode-mover = episodeMover.directory_tree_file_mover:main']
       }
 )
