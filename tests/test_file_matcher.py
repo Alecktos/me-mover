86917973ecf944739bf097f03133a4a606c0d434
Alecktos <persson33@gmail.com>
2016-12-30 17:18:03 +0100

    some renaming

--- tests/test_file_matcher.py
+++ tests/test_file_matcher.py
@@ -4,30 +4,30 @@ from memover import file_handler, file_matcher
 
 class FileMatcherTest(unittest.TestCase):
 
-    __TEST_SEARCH_IN_DIRECTORY_PATH = 'testfolder'
-    __TEST_FILE_PATH = __TEST_SEARCH_IN_DIRECTORY_PATH + '/The.Big.Bang.Theory.S09E01.something.something-something[something].mp4'
-    __TEST_FOLDER_PATH = __TEST_SEARCH_IN_DIRECTORY_PATH + '/hey.arnold.S09E01.SOMETHING.something-something'
+    __SOURCE_DIRECTORY_PATH = 'testfolder'
+    __TV_SHOW_1_FILE_PATH = __SOURCE_DIRECTORY_PATH + '/The.Big.Bang.Theory.S09E01.something.something-something[something].mp4'
+    __TV_SHOW_2_FOLDER_PATH = __SOURCE_DIRECTORY_PATH + '/hey.arnold.S09E01.SOMETHING.something-something'
 
     def setUp(self):
-        file_handler.create_dir(self.__TEST_SEARCH_IN_DIRECTORY_PATH)
-        file_handler.create_file(self.__TEST_FILE_PATH)
-        file_handler.create_dir(self.__TEST_FOLDER_PATH)
+        file_handler.create_dir(self.__SOURCE_DIRECTORY_PATH)
+        file_handler.create_file(self.__TV_SHOW_1_FILE_PATH)
+        file_handler.create_dir(self.__TV_SHOW_2_FOLDER_PATH)
 
     def tearDown(self):
-        file_handler.delete_directory(self.__TEST_SEARCH_IN_DIRECTORY_PATH)
+        file_handler.delete_directory(self.__SOURCE_DIRECTORY_PATH)
 
     def runTest(self):
         self.__test_matching_file()
         self.__test_matching_folder()
 
-    def __test_matching_file(self):
+    def __test_matching_files(self):
         search_for = 'The Big Bang Theory'
-        file_paths = file_matcher.search_files(search_for, self.__TEST_SEARCH_IN_DIRECTORY_PATH)
+        file_paths = file_matcher.search_files(search_for, self.__SOURCE_DIRECTORY_PATH)
         self.assertEquals(1, len(file_paths))
-        self.assertEqual(self.__TEST_FILE_PATH, file_paths[0])
+        self.assertEqual(self.__TV_SHOW_1_FILE_PATH, file_paths[0])
 
     def __test_matching_folder(self):
         search_for = 'hey arnold'
-        file_paths = file_matcher.search_files(search_for, self.__TEST_SEARCH_IN_DIRECTORY_PATH)
+        file_paths = file_matcher.search_files(search_for, self.__SOURCE_DIRECTORY_PATH)
         self.assertEquals(1, len(file_paths))
-        self.assertEqual(self.__TEST_FOLDER_PATH, file_paths[0])
+        self.assertEqual(self.__TV_SHOW_2_FOLDER_PATH, file_paths[0])
--- tests/test_media_file_extractor.py
+++ tests/test_media_file_extractor.py
@@ -5,43 +5,43 @@ from memover.media_file_extractor import MediaFileExtractor, Type, WrongMediatyp
 
 class MediaFileExtractorTest(unittest.TestCase):
 
-    tv_shows_paths = [
+    __TV_SHOWS_PATHS = [
         'testar/test/Falling.Skies.S02E03.720p.SOMETHING.SOMETHING-SOMETHING.mp4',
         'testar/intetest/The.Big.Bang.Theory.S09E01.something.something-something[something].mp4'
     ]
 
-    movie_paths = [
+    __MOVIE_PATHS = [
         'testar/test/The.Great.Gatsby.2012.BluRay.1080p.DTS.x224-AAA.mkv'
     ]
 
     def test_type(self):
-        media_file_extractor = MediaFileExtractor(self.tv_shows_paths[0])
+        media_file_extractor = MediaFileExtractor(self.__TV_SHOWS_PATHS[0])
         self.assertEqual(Type.TV_SHOW, media_file_extractor.get_type())
 
-        media_file_extractor = MediaFileExtractor(self.movie_paths[0])
+        media_file_extractor = MediaFileExtractor(self.__MOVIE_PATHS[0])
         self.assertEqual(Type.MOVIE, media_file_extractor.get_type())
 
-        media_file_extractor = MediaFileExtractor(self.tv_shows_paths[1])
+        media_file_extractor = MediaFileExtractor(self.__TV_SHOWS_PATHS[1])
         self.assertEqual(Type.TV_SHOW, media_file_extractor.get_type())
 
     def test_extract_episode(self):
-        media_file_extractor = MediaFileExtractor(self.tv_shows_paths[0])
+        media_file_extractor = MediaFileExtractor(self.__TV_SHOWS_PATHS[0])
         episode_number = media_file_extractor.get_episode_number()
         self.assertEqual('03', episode_number)
 
-        media_file_extractor = MediaFileExtractor(self.tv_shows_paths[1])
+        media_file_extractor = MediaFileExtractor(self.__TV_SHOWS_PATHS[1])
         self.assertEqual('01', media_file_extractor.get_episode_number())
 
-        media_file_extractor = MediaFileExtractor(self.movie_paths[0])
+        media_file_extractor = MediaFileExtractor(self.__MOVIE_PATHS[0])
         self.assertRaises(WrongMediatypeException, media_file_extractor.get_episode_number)
 
     def text_extract_season(self):
-        media_file_extractor = MediaFileExtractor(self.tv_shows_paths)
+        media_file_extractor = MediaFileExtractor(self.__TV_SHOWS_PATHS)
         season_number = media_file_extractor.get_season_number()
         self.assertEqual('09', season_number)
 
         season_number = media_file_extractor.get_season_number()
         self.assertEqual('02', season_number)
 
-        media_file_extractor = MediaFileExtractor(self.tv_shows_paths)
+        media_file_extractor = MediaFileExtractor(self.__TV_SHOWS_PATHS)
         self.assertRaises(WrongMediatypeException, media_file_extractor.get_season_number())
