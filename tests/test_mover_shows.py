5e3453a3cd59922a51382dcbfc3c1b8803f57a79
Alexander Persson <persson33@gmail.com>
2019-03-03 20:51:31 +0100

    extract shows and movies to different files for mover tests

--- tests/test_mover_shows.py
+++ tests/test_mover_shows.py
@@ -40,16 +40,6 @@ class TestMover(unittest.TestCase, file_mover_tester.FileMoverTester):
         destination_path = self._SHOW_DESTINATION_DIRECTORY + '/Man In The on Folder/Season 2/' + tv_show_file_name
         self._assert_file_moved(source_file_path, destination_path)
 
-    def test_move_movie_by_file_path(self):
-        movie_file_name = 'Abcde Fgh ijk Wjere To Found In 2010 HD-AA x111-ABC.mkv'
-        file_handler.create_file(self._SOURCE_DIRECTORY + '/' + movie_file_name)
-
-        source_file_path = self._SOURCE_DIRECTORY + '/' + movie_file_name
-        mover.move_media_by_path(source_file_path, self._SHOW_DESTINATION_DIRECTORY, self._MOVIE_DESTINATION_DIRECTORY)
-
-        destination_path = self._MOVIE_DESTINATION_DIRECTORY + '/' + file_handler.get_last_name_from_path(movie_file_name) + '/' + movie_file_name
-        self._assert_file_moved(source_file_path, destination_path)
-
     def test_move_shows_into_existing_season_directory(self):
         # Test with two letter word in the end of file name that should be stripped
         file_handler.create_dir(self._SHOW_DESTINATION_DIRECTORY + '/Kontroll/Season 7/')
@@ -121,48 +111,6 @@ class TestMover(unittest.TestCase, file_mover_tester.FileMoverTester):
         )
 
         self._assert_file_moved(file_path_1, self._SHOW_DESTINATION_DIRECTORY + 'Cool And Welcome To Earth/Season 1/Cool.And.Welcome.To.Earth.S01E13.720p.5.1Ch.Web-DL.ReEnc-DeeJayAhmed.mkv')
-
-    def test_moving_movie_in_directory(self):
-        folder_path = self._SOURCE_DIRECTORY + '007 Going For Old Time HD-TS x264-CPG'
-        file_handler.create_dir(folder_path)
-        file_path = folder_path + '/007 Going For Old Time  HD-TS x264-CPG.mp4'
-        file_handler.create_file(file_path)
-        mover.move_media_by_path(file_path, self._SHOW_DESTINATION_DIRECTORY, self._MOVIE_DESTINATION_DIRECTORY)
-        file_destination_path = self._MOVIE_DESTINATION_DIRECTORY + '/007 Going For Old Time  HD-TS x264-CPG/007 Going For Old Time  HD-TS x264-CPG.mp4'
-        self._assert_file_moved(file_path, file_destination_path)
-
-    def test_moving_movie_with_image(self):
-        folder_path = self._SOURCE_DIRECTORY + 'Konstig (2017) [1080p] [JKL.KL]'
-        file_handler.create_dir(folder_path)
-        source_file_path = folder_path + '/WWW.YTS.AG.jpg'
-        file_handler.create_file(source_file_path)
-
-        mover.move_media_by_path(folder_path, self._SHOW_DESTINATION_DIRECTORY, self._MOVIE_DESTINATION_DIRECTORY)
-
-        file_destination_path = self._MOVIE_DESTINATION_DIRECTORY + '/Konstig (2017) [1080p] [JKL.KL]/WWW.YTS.AG.jpg'
-        self._assert_file_moved(source_file_path, file_destination_path)
-
-    def test_moving_movies_by_name(self):
-        file_1_folder_path = self._SOURCE_DIRECTORY + '/201 Coolings 1999.DVDRIP.Xvid.NVesub-'
-        file_handler.create_dir(file_1_folder_path)
-        file_1_path = file_1_folder_path + '/201 Coolings 1999.___RIP.Xvid.NVesub-123.mp4'
-        file_handler.create_file(file_1_path)
-
-        file_2_path = self._SOURCE_DIRECTORY + '/201 Coolings 200.DADRAP.NVesub.mp4'
-        file_handler.create_file(file_2_path)
-
-        mover.move_media_by_name(
-            '201 Coolings',
-            self._SOURCE_DIRECTORY,
-            self._SHOW_DESTINATION_DIRECTORY,
-            self._MOVIE_DESTINATION_DIRECTORY
-        )
-
-        file_destination_path = self._MOVIE_DESTINATION_DIRECTORY + '/201 Coolings 1999.DVDRIP.Xvid.NVesub-/201 Coolings 1999.___RIP.Xvid.NVesub-123.mp4'
-        self._assert_file_moved(file_1_folder_path, file_destination_path)
-
-        file_destination_path = self._MOVIE_DESTINATION_DIRECTORY + '/201 Coolings 200.DADRAP.NVesub/201 Coolings 200.DADRAP.NVesub.mp4'
-        self._assert_file_moved(file_2_path, file_destination_path)
 
     def test_moving_wrong_formatted_episode_inside_right_formated_directory(self):
         folder_path = self._SOURCE_DIRECTORY + '/My.Stuff.Us.On.S10E09.HDTV.CCCC-ARD[axel]'
