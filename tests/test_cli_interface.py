509cca7e739bd240a8ad1aed984ca07143c0cbae
Alecktos <alexander.berlind@protonmail.com>
2020-01-06 22:13:50 +0100

    More tests for cli interface

--- tests/test_cli_interface.py
+++ tests/test_cli_interface.py
@@ -16,14 +16,32 @@ class TestSubtitles(unittest.TestCase, file_mover_tester.FileMoverTester):
     def tearDown(self):
         self._delete_test_dirs()
 
+    def test_watch(self):
+        args = 'watch testar testar2 testar3'
+        result = self.execute(args)
+        self.assertEqual(b'type: watch, source: testar, show_destination: testar2, movie_destination: testar3, quit None\n', result)
+
+    def test_watch_auto_quit(self):
+        args = 'watch testar testar2 testar3 -q 5'
+        result = self.execute(args)
+        self.assertEqual(b'type: watch, source: testar, show_destination: testar2, movie_destination: testar3, quit 5\n', result)
+
+    def test_by_name(self):
+        args = 'by-name testar testar2 testar3'
+        result = self.execute(args)
+        self.assertEqual(b'type: by-name, source: testar, show_destination: testar2, movie_destination: testar3, quit None\n', result)
+
     def test_by_path(self):
         args = 'by-path testar testar2 testar3'
+        result = self.execute(args)
+        self.assertEqual(b'type: by-path, source: testar, show_destination: testar2, movie_destination: testar3, quit None\n', result)
 
+    def execute(self, args):
         p = subprocess.Popen(f'{sys.executable} test_cli_interface.py {args}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = p.communicate()
         result = output[0]
         print(result)
-        self.assertEqual(b'type: by-path, source: testar, show_destination: testar2, movie_destination: testar3, quit None\n', result)
+        return result
 
 
 if __name__ == '__main__':
