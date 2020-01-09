0c2711bb1f768469c69d9740d658c22d7efdf6ce
Alecktos <alexander.berlind@protonmail.com>
2020-01-09 15:39:19 +0100

    Change version to 3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ __author__ = 'alexander.berlind'
 from setuptools import setup
 
 setup(name='me-mover',
-      version='0.2dev',
+      version='3.0',
       packages=['memover'],
       author='Alexander Berlind',
       url='https://github.com/Alecktos/me-mover.git',
