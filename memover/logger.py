28dd87c043720547edf01e2a23e1ec6d7f4b9148
Alecktos <alexander.berlind@protonmail.com>
2020-02-09 17:35:20 +0100

    Remove name

--- memover/logger.py
+++ memover/logger.py
@@ -6,7 +6,7 @@ Log = logging.getLogger("me-mover")
 def setup(level: int):
     logging.basicConfig(
         level=level,
-        format="%(asctime)s %(levelname)-8s %(name)s %(message)s",
+        format="%(asctime)s %(levelname)-8s %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S")
 
     logging.getLogger('watchdog').setLevel(logging.INFO)
