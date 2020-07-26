a2b43c99dc80a4472b8441b6a27ab26bdbe8c6e2
Alecktos <alexander.berlind@protonmail.com>
2020-07-26 19:52:29 +0200

    Start using moves flag instead of lifetime in tests

--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -4,28 +4,17 @@ import time
 import unittest
 
 from memover import file_handler
-from memover.arguments_parser import MeMoverArgs, Commands
 from tests.utils import file_mover_tester
 
 
 class TestWatcher(unittest.TestCase, file_mover_tester.FileMoverTester):
 
-
-    __auto_turn_off = 4  # in seconds
     __test_file_1 = 'kolla.S04E15.asswe.xTTT-RR[abf].mkv'
     __test_file_2 = 'kolla.S05E16.asswe.xTTT-RR[abf].mkv'
     __args = None
 
-
     def setUp(self):
         self._create_test_dirs()
-        self.__args = MeMoverArgs(
-            Commands.WATCH,
-            self._SOURCE_DIRECTORY,
-            self._SHOW_DESTINATION_DIRECTORY,
-            self._MOVIE_DESTINATION_DIRECTORY,
-            self.__auto_turn_off
-        )
 
     def tearDown(self):
         self._delete_test_dirs()
@@ -38,7 +27,7 @@ class TestWatcher(unittest.TestCase, file_mover_tester.FileMoverTester):
 
     def test_moving_multiple_files(self):
         def run_app():
-            args = f'{self._SOURCE_DIRECTORY} {self._SHOW_DESTINATION_DIRECTORY} {self._MOVIE_DESTINATION_DIRECTORY} --lifetime {self.__auto_turn_off}'
+            args = f'{self._SOURCE_DIRECTORY} {self._SHOW_DESTINATION_DIRECTORY} {self._MOVIE_DESTINATION_DIRECTORY} --moves 2'
             execution = f'{sys.executable} -m memover watch {args}'
             p = subprocess.Popen(execution, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
             return p
