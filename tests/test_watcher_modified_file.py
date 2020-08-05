c6c77bc75fd50c013096d5b4a73c02133eb0ac2c
Alecktos <alexander.berlind@protonmail.com>
2020-08-05 17:49:57 +0200

    Make growing file more robust and test running it on github

--- tests/test_watcher_modified_file.py
+++ tests/test_watcher_modified_file.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import unittest
 
+from memover import logger
 from memover.arguments_parser import MeMoverArgs, Commands
 from memover.watcher.async_watcher import AsyncWatcher
 from tests.utils import file_mover_tester
@@ -18,47 +19,45 @@ class TestWatcherModifiedFile(unittest.TestCase, file_mover_tester.FileMoverTest
         self._delete_test_dirs()
 
     async def make_file_bigger(self):
-        await asyncio.sleep(1)
-
         # modify and create file
         self._createSourceFile(test_file_1)
         self._set_size_in_mb(test_file_1, 1)
 
         await asyncio.sleep(1)
 
-        # modify file
-        self._set_size_in_mb(test_file_1, 1)
-
-        self.assertTrue(self.__test_file_1_path in self.my_watcher.modified_paths)
-        self.assertTrue(self.__test_file_1_path in self.my_watcher.created_paths)
-
-        await asyncio.sleep(1)
-
-        self.assertTrue(self.__test_file_1_path in self.my_watcher.modified_paths)
+        # Can't assert modified_queue because it might already have been removed from queue
+        # But file should never disappear from created queue if we keep modifying it
+        # self.assertTrue(self.__test_file_1_path in self.my_watcher.modified_paths)
         self.assertTrue(self.__test_file_1_path in self.my_watcher.created_paths)
 
         # modify file
         self._set_size_in_mb(test_file_1, 1)
-
         await asyncio.sleep(1)
 
-        self.assertTrue(self.__test_file_1_path in self.my_watcher.modified_paths)
         self.assertTrue(self.__test_file_1_path in self.my_watcher.created_paths)
 
-        # no modification to file
+        # modify file
+        self._set_size_in_mb(test_file_1, 1)
+
         await asyncio.sleep(1)
 
-        self.assertFalse(self.__test_file_1_path in self.my_watcher.modified_paths)
         self.assertTrue(self.__test_file_1_path in self.my_watcher.created_paths)
 
-        # No modification so should be moved
-        await asyncio.sleep(1)
+        # No modification to file.
+        # Sleep 2 seconds to be sure that file has been moved
+        await asyncio.sleep(2)
 
         self.assertFalse(self.__test_file_1_path in self.my_watcher.modified_paths)
         self.assertFalse(self.__test_file_1_path in self.my_watcher.created_paths)
 
-    @unittest.skip("Does not work on github")
+        self._assert_file_moved(
+            self.__test_file_1_path,
+            f'{self._SHOW_DESTINATION_DIRECTORY}/kolla/Season 4/{test_file_1}'
+        )
+
+
     def test_moving_growing_file(self):
+        logger.setup(logging.DEBUG)
         self.my_watcher = None
         self.__test_file_1_path = f'{self._SOURCE_DIRECTORY}{test_file_1}'
 
@@ -77,8 +76,8 @@ class TestWatcherModifiedFile(unittest.TestCase, file_mover_tester.FileMoverTest
 
         async def run_watcher_with_growing_file():
             await asyncio.gather(
-                self.make_file_bigger(),
-                self.my_watcher.observe()
+                self.my_watcher.observe(),
+                self.make_file_bigger()
             )
 
         asyncio.run(run_watcher_with_growing_file())
