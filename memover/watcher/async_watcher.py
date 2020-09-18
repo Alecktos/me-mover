fdaa8baec977fd05f1bb64d253412c6e2044e898
Alecktos <alexander.berlind@protonmail.com>
2020-09-18 12:43:08 +0200

    Removed unused listeners

--- memover/watcher/async_watcher.py
+++ memover/watcher/async_watcher.py
@@ -33,14 +33,8 @@ class AsyncWatcher:
     def on_created(self, event):
         self.__synced_watcher.on_created(event.src_path)
 
-    def on_deleted(self, event):
-        pass
-
     def on_modified(self, event):
         self.__synced_watcher.on_modified(event.src_path)
-
-    def on_moved(event):
-        pass
 
     async def observe(self):
         patterns = "*"
@@ -50,9 +44,7 @@ class AsyncWatcher:
 
         my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns, ignore_directories, case_sensitive)
         my_event_handler.on_created = self.on_created
-        my_event_handler.on_deleted = self.on_deleted
         my_event_handler.on_modified = self.on_modified
-        my_event_handler.on_moved = self.on_moved
 
         path = self.__args.source
         logger.info(f"Watching: {path}")
