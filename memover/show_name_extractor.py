b9d7a1df41ac88f29ef65416f94c422e68ef16b9
Alecktos <alexander.berlind@protonmail.com>
2021-01-05 13:52:45 +0100

    Rename extract_show_name helper methods

--- memover/show_name_extractor.py
+++ memover/show_name_extractor.py
@@ -7,10 +7,10 @@ def extract_show_name(show_name):
         .replace('.', ' ') \
         .split()
 
-    show_name_words = __extract_release_date(show_name_words)
-    show_name_words = __extract_sample_word(show_name_words)
-    show_name_words = __extract_garbage_words(show_name_words)
-    show_name_words = __extract_meta_info(show_name_words)
+    show_name_words = __remove_release_date(show_name_words)
+    show_name_words = __remove_sample_word(show_name_words)
+    show_name_words = __remove_garbage_words(show_name_words)
+    show_name_words = __remove_meta_info(show_name_words)
     show_name_words = __trim_garbage_chars(show_name_words)
     show_name_words = __strip_from_season(show_name_words)
     extracted_show_name = ' '.join(show_name_words)
@@ -29,7 +29,7 @@ def __extract_urls(show_name):
     return __extract_urls(show_name)
 
 
-def __extract_release_date(show_name_words):
+def __remove_release_date(show_name_words):
     d1 = datetime.date(2000, 1, 1)
     d2 = datetime.date.today()
 
@@ -41,11 +41,11 @@ def __extract_release_date(show_name_words):
         return show_name_words
 
 
-def __extract_sample_word(show_name_words):
+def __remove_sample_word(show_name_words):
     return [re.sub('\W*sample\W*', '', word, 0, re.IGNORECASE) for word in show_name_words]
 
 
-def __extract_garbage_words(show_name_words):
+def __remove_garbage_words(show_name_words):
     garbage_words = ['title']
     lower_cases = list(map(str.lower, show_name_words))
 
@@ -58,12 +58,12 @@ def __extract_garbage_words(show_name_words):
     return show_name_words
 
 
-def __extract_meta_info(show_name_words):
+def __remove_meta_info(show_name_words):
     if '[' in show_name_words and ']' in show_name_words:
         start_meta_info = show_name_words.index('[')
         end_meta_info = show_name_words.index(']')
         del show_name_words[start_meta_info:end_meta_info + 1]
-        return __extract_meta_info(show_name_words)
+        return __remove_meta_info(show_name_words)
 
     return show_name_words
 
@@ -80,7 +80,7 @@ def __trim_garbage_chars(show_name_words):
 
     if show_name_words[-1] in garbage_chars:
         show_name_words.remove(show_name_words[-1])
-        return __extract_meta_info(show_name_words)
+        return __remove_meta_info(show_name_words)
 
     return show_name_words
 
